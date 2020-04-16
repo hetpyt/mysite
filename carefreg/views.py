e65f4687ca86ffc8a44a98be78b7433d7ee8eb66
hetpyt <43849401+hetpyt@users.noreply.github.com>
2020-04-16 16:24:57 +0300

    views

--- carefreg/views.py
+++ carefreg/views.py
@@ -7,7 +7,11 @@ class IndexView(generic.ListView):
     template_name = 'carefreg/index.html'
     context_object_name = 'provided_service_list'
     extra_context = {'page_title' : "Перечень оказанных услуг",}
-
+    fields = {}
+    for field in ProvidedServices._meta.local_fields:
+        fields[field.name] = field.verbose_name
+    extra_context['model_fields'] = fields   
+    
     def get_queryset(self):
 	    return ProvidedServices.objects.order_by('-service_date')
 
