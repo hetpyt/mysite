f0d66879a4db31897bf3b3bf8bfc9dcdd9e15ee6
hetpyt <43849401+hetpyt@users.noreply.github.com>
2020-04-22 18:36:21 +0300

    models

--- carefreg/admin.py
+++ carefreg/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
-from .models import DeviceModel, CartridgeModel, Device, Cartridge, Service, ProvidedServices
+from .models import *
 # Register your models here.
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_model', 'device_name', 'serial_number', 'production_date', 'start_date', 'current_department_name')
 
+class CartridgeAdmin(admin.ModelAdmin):
+    list_display = ('cartridge_model', 'serial_number', 'start_date', 'current_device_name', 'current_department_name', )
+    
 class ProvidedServicesAdmin(admin.ModelAdmin):
     list_display = ('service_date', 'cartridge', 'service', 'service_price', 'invoice_num', 'invoice_date')
     list_filter = ['service_date']
     search_fields = ['servise']
 
+class RelCartridgeDeviceAdmin(admin.ModelAdmin):
+    list_display = ('rel_date', 'cartridge', 'owner_device', 'comment')
 
+class RelDeviceDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('rel_date', 'device', 'owner_dept', 'comment')
+    
 admin.site.register(DeviceModel)
 admin.site.register(CartridgeModel)
-admin.site.register(Device)
-admin.site.register(Cartridge)
+admin.site.register(Department)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Cartridge, CartridgeAdmin)
 admin.site.register(Service)
+admin.site.register(RelCartridgeDevice, RelCartridgeDeviceAdmin)
+admin.site.register(RelDeviceDepartment, RelDeviceDepartmentAdmin)
 admin.site.register(ProvidedServices, ProvidedServicesAdmin)
--- carefreg/models.py
+++ carefreg/models.py
@@ -1,25 +1,37 @@
 from django.db import models
-# Create your models here.
+from django.utils import timezone
 # !!!есть картриджи, которые можно изпользовать на разных устройствах одного производителя
 
-class DeviceModel(models.Model):
+class AbstractDeviceModel(models.Model):
     vendor_name = models.CharField('Производитель', max_length = 50, blank = False, unique = False)
     model_name = models.CharField('Модель', max_length = 50, blank = False, unique = False)
-    
+
     def __str__(self):
         return f"{self.vendor_name} {self.model_name}"
+
+    class Meta:
+        abstract = True
+
+class AbstractRelationship(models.Model):
+    rel_date = models.DateField('Дата', db_index = True, null = False, blank = False)
+    comment = models.CharField('Комментарий', max_length = 150, blank = True)
     
-class CartridgeModel(models.Model):
-    vendor_name = models.CharField('Производитель', max_length = 50, blank = False, unique = False)
-    model_name = models.CharField('Модель', max_length = 50, blank = False, unique = False)
+    class Meta:
+        abstract = True
+        
+class DeviceModel(AbstractDeviceModel):
+    pass
     
-    def __str__(self):
-        return f"{self.vendor_name} {self.model_name}"
- 
+class CartridgeModel(AbstractDeviceModel):
+    pass
+    
 class Department(models.Model):
     department_name = models.CharField('Наименование отдела', max_length = 50, blank = False, unique = False)
-    abbreviated_name = models.CharField('Сокращенное наименование', max_length = 3, blank = False, unique = True)
- 
+    abbreviated_name = models.CharField('Аббревиатура', max_length = 3, blank = False, unique = True)
+
+    def __str__(self):
+        return f"{self.department_name} ({self.abbreviated_name})"
+        
 class Device(models.Model):
     device_model = models.ForeignKey(DeviceModel, on_delete = models.CASCADE, verbose_name = 'Модель устройства')
     serial_number = models.CharField('Серийный номер', max_length = 50, blank = False, unique = True)
@@ -35,6 +47,16 @@ class Device(models.Model):
         else:
             return f"{self.device_model}"
 
+    def current_department_name(self):
+        now = timezone.now()
+        rel = RelDeviceDepartment.objects.filter(device = self.id, rel_date__lte = now).select_related('owner_dept').order_by('-rel_date')
+        if rel.exists():
+            return str(rel[0].owner_dept)
+        else:
+            return ''
+            
+    current_department_name.short_description = 'Текущий отдел'
+    
 class Cartridge(models.Model):
     cartridge_model = models.ForeignKey(CartridgeModel, on_delete = models.CASCADE, verbose_name = 'Модель картриджа')
     serial_number = models.CharField('Серийный номер', max_length = 50, blank = False, unique = True)
@@ -44,13 +66,61 @@ class Cartridge(models.Model):
     def __str__(self):
         return f"{self.cartridge_model} [{self.serial_number}]"
 
+    def current_department_name(self):
+        return ''
+        
+    def current_device_name(self):
+        now = timezone.now()
+        rel_dev_qs = RelCartridgeDevice.objects.filter(cartridge = self.id, rel_date__lte = now).select_related('owner_device').order_by('-rel_date')
+        if rel_dev_qs.exists():
+            
+            def get_current_department_name():
+                rel_dept_qs = RelDeviceDepartment.objects.filter(device = rel_dev_qs[0].owner_device.id, rel_date__lte = now).select_related('owner_dept').order_by('-rel_date')
+                if rel_dept_qs.exists():
+                    return str(rel_dept_qs[0].owner_dept)
+                else:
+                    return ''
+            self.current_department_name = get_current_department_name
+            
+            return str(rel_dev_qs[0].owner_device)
+        else:
+            return ''
+    
+    current_department_name.short_description = 'Текущий отдел'
+    current_device_name.short_description = 'Текущее устройство'
+
+
 class Service(models.Model):
     service_name = models.CharField('Наименование услуги', max_length = 100, blank = False, unique = True)
     default_price = models.DecimalField('Цена по умолчанию', max_digits = 10, decimal_places = 2, null = False, blank = True)
     
     def __str__(self):
         return self.service_name
+
+class RelCartridgeDevice(AbstractRelationship):
+    """ Связи между картриджами и устройствами, в которых они используются, во времени """
+    owner_device = models.ForeignKey(Device, on_delete = models.CASCADE, verbose_name = 'Устройство', null = False, blank = False)
+    cartridge = models.ForeignKey(Cartridge, on_delete = models.CASCADE, verbose_name = 'Картридж', null = False, blank = False)
     
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields = ['rel_date', 'cartridge'], name = 'rel_cartridge_device__unique_date_cartridge'),
+            models.UniqueConstraint(fields = ['rel_date', 'cartridge', 'owner_device'], name = 'rel_cartridge_device__unique_date_cartridge_owner_device'),
+        ]
+        ordering = ('rel_date',)
+
+class RelDeviceDepartment(AbstractRelationship):
+    """ Связи между устройствами и отделами, в которых они используются, во времени """
+    owner_dept = models.ForeignKey(Department, on_delete = models.CASCADE, verbose_name = 'Отдел', null = False, blank = False)
+    device = models.ForeignKey(Device, on_delete = models.CASCADE, verbose_name = 'Устройство', null = False, blank = False)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields = ['rel_date', 'device'], name = 'rel_device_department__unique_date_device'),
+            models.UniqueConstraint(fields = ['rel_date', 'device', 'owner_dept'], name = 'rel_device_department__unique_date_device_owner_dept'),
+        ]
+        ordering = ('rel_date',)
+        
 class ProvidedServices(models.Model):
     service_date = models.DateField('Дата услуги', null = False, blank = False)
     cartridge = models.ForeignKey(Cartridge, on_delete = models.CASCADE, verbose_name = 'Картридж', null = False, blank = False)
@@ -62,3 +132,5 @@ class ProvidedServices(models.Model):
     def __str__(self):
         return f"{self.service_date} {self.service} {self.cartridge}"
     
+__all__ = ['DeviceModel', 'CartridgeModel', 'Department', 'Device', 'Cartridge', 'Service',
+    'RelCartridgeDevice', 'RelDeviceDepartment', 'ProvidedServices']
